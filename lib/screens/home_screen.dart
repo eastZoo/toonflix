a1066582450be560f3105a87e2a889d00e61d7bc
eastzoo <dongju010@kakao.com>
2023-06-15 21:53:30 +0900

    UPDATE : 타이머 스탑 함수, 스탑 아이콘

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  bool isRunning = false;
   int totalSeconds = 1500;
   // late = 이 property를 당장 초기화 하지 않아도 된다
   // 하지만 사용하기 전에 반드시 초기화 한다고 약속!!!
@@ -28,6 +29,16 @@ class _HomeScreenState extends State<HomeScreen> {
       const Duration(seconds: 1),
       onTick,
     );
+    setState(() {
+      isRunning = true;
+    });
+  }
+
+  void onPausePressed() {
+    timer.cancel();
+    setState(() {
+      isRunning = false;
+    });
   }
 
   @override
@@ -57,8 +68,10 @@ class _HomeScreenState extends State<HomeScreen> {
               child: IconButton(
                 iconSize: 120,
                 color: Theme.of(context).cardColor,
-                onPressed: onStartPressed,
-                icon: const Icon(Icons.play_circle_outline),
+                onPressed: isRunning ? onPausePressed : onStartPressed,
+                icon: Icon(isRunning
+                    ? Icons.pause_circle_outline
+                    : Icons.play_circle_outline),
               ),
             ),
           ),
