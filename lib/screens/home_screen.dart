202cf65b70946f2def9aaba2c5262d7584d00f20
eastzoo <dongju010@kakao.com>
2023-06-21 20:39:54 +0900

    UPDATE : 이전 커밋의 데이터 페칭 에서 더 나아간 Stateless위젯으로 데이터 페칭

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,35 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  // 아래와 같이 데이터를 받아오는 방법은 변수를( webtoons, isLoading ) 만들고, 수동 await, setState 사용하고 등.. 할일이 너무많고 반복적인 작업이 될 수 있다..
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    waitForWebToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
     print(webtoons);
-    print(isLoading);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
@@ -45,6 +24,18 @@ class _HomeScreenState extends State<HomeScreen> {
             fontWeight: FontWeight.bold,
           ),
         ),
+      ),
+      // FutureBuilder 는 stateless위젯에서 Future타입을 기다렸다가 빌드해준다.!!
+      body: FutureBuilder(
+        // future에 받아와야할 비동기 데이터를 적어주면 보이진 않지만 자동 await
+        future: webtoons,
+        //snapshot을 통해 상태를 알 수 있다.( error, data, state,,,)
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text("There is data!");
+          }
+          return const Text('Loading...');
+        },
       ),
     );
   }
